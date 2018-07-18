1c02b8a06aed153e70e685d79b573fb3ff78a585
Nicholas Muturi <nicholasmuturi1@gmail.com>
2018-07-18 22:00:17 +0300

    checkbox selects individual list items

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,23 @@ $(document).ready(function() {
     $("ul#listDisplay").append('<li class="item active"><input type="checkbox" class="lsItem"> '+listItem+'</li>');
     $("input#todoItem").val("");
 
-  });
+    // $("input.lsItem").last().change(function(){
+    //   var input = $(this);
+    //   if(input.last().prop("checked")){
+    //     $(".item").last().toggleClass("inactive");
+    //   }
+    //   else{
+    //     $(".item").last().toggleClass("inactive");
+    //   }
+    // });
+    $('input.lsItem').last().change(function(){
+      if($('input.lsItem').is(':checked')){
+        console.log(listItem + " is selected");
+      }else{
+        console.log(listItem + " is not selected");
+      }
+    });
 
-  $("input.lsItem").change(function(){
-    var input = $(this);
-    if(input.last().prop("checked")){
-      $(".item").last().toggleClass("inactive");
-    }else {
-      $(".item").last().toggleClass("inactive");
-    }
   });
 
 });
